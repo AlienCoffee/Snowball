e7ca17489c9510ae2afd17331ceb249cebf69232
Shemplo <shemplo@outlook.com>
2018-10-08 21:20:13 +0300

    Builder extended

--- src/ru/shemplo/snowball/utils/db/DBQueryBuilder.java
+++ src/ru/shemplo/snowball/utils/db/DBQueryBuilder.java
@@ -1,13 +1,19 @@
 package ru.shemplo.snowball.utils.db;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
+
+import ru.shemplo.snowball.stuctures.Pair;
 
 public abstract class DBQueryBuilder {
 	
 	private DBQueryBuilder () {}
 	
 	public abstract DBQuery asQuery ();
+	
+	//-------------------------------//
 	
 	//-------------------------------//
 	
@@ -48,7 +54,7 @@ public abstract class DBQueryBuilder {
 			DBValidator.testName (name, this);
 			
 			StringBuilder sb = new StringBuilder ();
-			sb.append ("`").append (name).append ("` ").append (type);
+			sb.append ("'").append (name).append ("' ").append (type);
 			if (isNotNull) {
 				sb.append (" NOT NULL");
 			}
@@ -57,6 +63,18 @@ public abstract class DBQueryBuilder {
 		}
 		
 	}
+	
+	/* 
+	 * ╔═════════════════════╗
+	 * ║                     ║
+	 * ║ CREATE BUILDER PART ║
+	 * ║                     ║
+	 * ╚═════════════════════╝
+	 * 
+	 * Documentation:
+	 * https://phpclub.ru/mysql/doc/create-table.html
+	 * 
+	 */
 	
 	public static CreateBuilder buildCREATE () {
 		return new CreateBuilder ();
@@ -69,27 +87,42 @@ public abstract class DBQueryBuilder {
 		public CreateTableBuilder table (String name) { return new CreateTableBuilder (name); }
 		public CreateTableBuilder table (           ) { return new CreateTableBuilder ("");   }
 		
+		
+		
 	}
 	
 	public static class CreateTableBuilder extends DBQueryBuilder {
 
 		private final List <ColumnBuilder <CreateTableBuilder>> 
 			COLUMNS = new ArrayList <> ();
+		private final List <Pair <DBKey, String []>>
+			KEYS = new ArrayList <> ();
 		
-		private boolean onlyOnAbset = false;
-		private String name;
+		private boolean onlyOnAbset = false, 
+						isTemporary = false;
+		private String name, like = null;
 		
 		private CreateTableBuilder (String name) {
 			this.name = name;
 		}
 		
-		public CreateTableBuilder setName (String name) {
-			this.name = name;
+		public CreateTableBuilder temporary () {
+			this.isTemporary = true;
 			return this;
 		}
 		
 		public CreateTableBuilder onAbsent () {
 			this.onlyOnAbset = true;
+			return this;
+		}
+		
+		public CreateTableBuilder name (String name) {
+			this.name = name;
+			return this;
+		}
+		
+		public CreateTableBuilder like (String name) {
+			this.like = name;
 			return this;
 		}
 		
@@ -114,29 +147,113 @@ public abstract class DBQueryBuilder {
 		}
 		
 		public CreateTableBuilder key (DBKey key, String ... columns) {
+			KEYS.add (Pair.mp (key, columns));
 			return this;
 		}
 		
 		@Override
 		public DBQuery asQuery () {
 			DBValidator.testName (name, this);
+			DBValidator.testColumns (COLUMNS);
 			
-			StringBuilder sb = new StringBuilder ("CREATE TABLE ");
-			if (onlyOnAbset) {
-				sb.append (" IF NOT EXISTS ");
+			StringBuilder sb = new StringBuilder ("CREATE ");
+			if (isTemporary) {
+				sb.append ("TEMPORARY ");
 			}
-			sb.append ("`").append (name).append ("`");
 			
-			sb.append ("(");
-			for (int i = 0; i < COLUMNS.size (); i++) {
-				sb.append (COLUMNS.get (i).asQuery ());
-				if (i < COLUMNS.size () - 1) {
+			sb.append ("TABLE ");
+			if (onlyOnAbset) {
+				sb.append ("IF NOT EXISTS ");
+			}
+			
+			sb.append ("'").append (name).append ("' ");
+			if (like != null && like.length () > 0) {
+				sb.append ("LIKE `").append (like).append ("`");
+			} else {
+				sb.append ("(");
+				for (int i = 0; i < COLUMNS.size (); i++) {
+					sb.append (COLUMNS.get (i).asQuery ());
+					if (i < COLUMNS.size () - 1) {
+						sb.append (", ");
+					}
+				}
+				
+				if (KEYS.size () > 0) {
 					sb.append (", ");
 				}
+				for (int i = 0; i < KEYS.size (); i++) {
+					Pair <DBKey, String []> key = KEYS.get (i);
+					sb.append (key.F.name ()).append (" KEY (");
+					StringJoiner joiner = new StringJoiner (", ");
+					Arrays.asList (key.S).stream ()
+						  .map (s -> "'" + s + "'")
+						  .forEach (joiner::add);
+					sb.append (joiner).append (")");
+					if (i < COLUMNS.size () - 1) {
+						sb.append (", ");
+					}
+				}
+				sb.append (")");
 			}
-			sb.append (")");
 			
 			return new DBQuery (sb.toString ());
+		}
+		
+	}
+	
+	/* 
+	 * ╔═════════════════════╗
+	 * ║                     ║
+	 * ║ SELECT BUILDER PART ║
+	 * ║                     ║
+	 * ╚═════════════════════╝
+	 * 
+	 * Documentation:
+	 * https://phpclub.ru/mysql/doc/select.html
+	 * 
+	 */
+	
+	public static SelectBuilder buildSELECT () {
+		return new SelectBuilder ();
+	}
+	
+	public static class SelectBuilder extends DBQueryBuilder {
+		
+		private SelectBuilder () {}
+
+		public SelectBuilder from () {
+			return this;
+		}
+		
+		public SelectBuilder straightJoin () {
+			return this;
+		}
+		
+		public SelectBuilder sqlSmallResult () {
+			return this;
+		}
+		
+		public SelectBuilder sqlBigResult () {
+			return this;
+		}
+		
+		public SelectBuilder sqlBufferResult () {
+			return this;
+		}
+		
+		public SelectBuilder sqlCache (boolean useCache) {
+			return this;
+		}
+		
+		public SelectBuilder filter (DBFilter filter) {
+			return this;
+		}
+		
+		// TODO: select expression is next
+		
+		@Override
+		public DBQuery asQuery () {
+			return null;
 		}
 		
 	}
--- src/ru/shemplo/snowball/utils/db/DBValidator.java
+++ src/ru/shemplo/snowball/utils/db/DBValidator.java
@@ -1,10 +1,22 @@
 package ru.shemplo.snowball.utils.db;
 
+import java.util.List;
+
+import ru.shemplo.snowball.utils.db.DBQueryBuilder.ColumnBuilder;
+import ru.shemplo.snowball.utils.db.DBQueryBuilder.CreateTableBuilder;
+
 public class DBValidator {
 
 	public static void testName (String name, DBQueryBuilder owner) {
 		if (name == null || name.length () == 0) {
 			String text = "Name of " + owner.getClass ().getSimpleName () + " is NULL or empty";
+			throw new IllegalStateException (text);
+		}
+	}
+	
+	public static void testColumns (List <ColumnBuilder <CreateTableBuilder>> columns) {
+		if (columns == null || columns.size () == 0) {
+			String text = "Table must contains at least one column";
 			throw new IllegalStateException (text);
 		}
 	}
