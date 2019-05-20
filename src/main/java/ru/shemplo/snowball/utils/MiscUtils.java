feae2eae52963225c96e92a16a17b8bf14e4d13a
Plotnikov Andrey <shemplo@outlook.com>
2019-05-20 09:31:48 +0300

    New method `get current method`

--- src/main/java/ru/shemplo/snowball/utils/MiscUtils.java
+++ src/main/java/ru/shemplo/snowball/utils/MiscUtils.java
@@ -1,11 +1,29 @@
 package ru.shemplo.snowball.utils;
 
+import java.lang.reflect.Method;
 
 public class MiscUtils {
     
     @SuppressWarnings ("unchecked")
     public static <I, R> R cast (I object) {
         return (R) object;
+    }
+    
+    public static Method getMethod () {
+        StackTraceElement need = Thread.currentThread ()
+                               . getStackTrace () [2];
+        try {            
+            final Class <?> type = Class.forName (need.getClassName ());
+            for (Method method : type.getDeclaredMethods ()) {
+                if (method.getName ().equals (need.getMethodName ())) {
+                    return method;
+                }
+            }
+        } catch (ClassNotFoundException cnfe) {
+            return null;
+        }
+        
+        return null;
     }
     
 }
